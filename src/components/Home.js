66d6fd837d6636f6f7b9d8e215e9738085b4d7f6
DevanshuSawarkar <sawarkardevanshu@gmail.com>
2024-11-13 13:26:49 +0530

    updating navbar button reposiveness

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,6 +13,9 @@ export default function Home() {
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon"></span>
           </button>
